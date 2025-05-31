df4c168f1f6d088e7eada1702c542f640fac0371
PickStars <pickstars2024@outlook.com>
2025-05-31 09:29:35 +0800

    - (Repair) Added support for input versions above 1.20

--- src/main/java/com/pickstars/bss/BetterSlapSynthesis.java
+++ src/main/java/com/pickstars/bss/BetterSlapSynthesis.java
@@ -2,24 +2,41 @@ package com.pickstars.bss;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.ShapedRecipe;
+import net.minecraft.recipe.ShapelessRecipe;
+import net.minecraft.recipe.book.RecipeBook;
+import net.minecraft.recipe.book.RecipeBookCategory;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Locale;
+
 public class BetterSlapSynthesis implements ModInitializer {
-	public static final String MOD_ID = "betterslapsynthesis";
+    public static final String MOD_ID = "betterslapsynthesis";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-		LOGGER.info("Hello Better Slap Synthesis!");
-	}
+        LOGGER.info("Hello Fabric world!");
+        LOGGER.info("Hello Better Slap Synthesis!");
+    }
 }
